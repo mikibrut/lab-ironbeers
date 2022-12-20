8ae2f099b92cd7ebcee6f00a3770a19ee11b101b
Miki Brut <martiejarque@gmail.com>
2022-12-20 13:13:42 +0100

    lab done

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+
+
 
 // ...
 
@@ -20,6 +22,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {console.log(beers[0])
+    res.render('beers', {b: beers})
+  })
+  .catch(error => {console.log(error)
+    res.render('page500')
+  })
+});
+
+
+app.get('/randomBeers', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeers', {beer: beers[0]})
+  })
+  .catch(error => console.log(error))
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
